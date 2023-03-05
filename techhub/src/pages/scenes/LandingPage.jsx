84c7ff2ebe6bb97fbcafe0f833737d802e308739
Wael Hourani <wael.business@outlook.com>
2023-03-05 11:03:31 +1100

    commit

--- techhub/src/pages/scenes/LandingPage.jsx
+++ techhub/src/pages/scenes/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import landinglogo from "../../../landinglogo.png";
+import landinglogo from "../../public/landinglogo.png";
+
+import Image from "next/image";
 
 const LandingPage = () => {
   return (
     <section className="flex justify-center items-center h-[800px]">
       <div className=" bg-neogreen border-solid border-t-4 border-r-[12px] border-b-[12px] border-l-4 border-black shadow-4xl p-10 rounded-full z-0">
-        <img src={landinglogo} alt="THE TECH HUB" className="z-5" />
+        <Image src={landinglogo.src} alt="THE TECH HUB" />
       </div>
     </section>
   );
