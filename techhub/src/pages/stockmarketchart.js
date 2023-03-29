3e0d2179f418bdbcefae9a52ce554df64969cfca
Wael Hourani <wael.business@outlook.com>
2023-03-29 13:50:26 +1100

    commit

--- techhub/src/pages/stockmarketchart.js
+++ techhub/src/pages/stockmarketchart.js
@@ -17,41 +17,74 @@ export async function getServerSideProps () {
 }
 
 const StockMarketCharts = ({chartData}) => {
- ;
+  const SYMBOL = "TSLA";
 
-  
-  useEffect(() => {
-    const labels = [];
-    const prices = [];
-    for (let key in chartData) {
-      labels.push(key);
-      prices.push(chartData[key]["4. close"]);
+  useEffect(() => { 
+    const labels = []; // Creating an empty array to hold the x-axis labels for the chart
+    const prices = []; // Creating an empty array to hold the y-axis prices for the chart
+    const timeSeries = chartData["Time Series (60min)"]; // Extracting the "Time Series (60min)" object from the chartData prop
+    for (let key in timeSeries) { // Looping through each key in the "Time Series (60min)" object
+      labels.push(key); // Adding the key (which represents a date and time) to the labels array
+      prices.push(timeSeries[key]["4. close"]); // Adding the closing price for the key's date and time to the prices array
     }
-    const ctx = document.getElementById("stockChart");
-    if (window.stockChart instanceof Chart) {
-      window.stockChart.destroy();
+    const ctx = document.getElementById("stockChart"); // Getting the canvas element with an ID of "stockChart"
+    if (window.stockChart instanceof Chart) { // Checking if there is already a chart instance stored in the global window object
+      window.stockChart.destroy(); // If there is, destroy it to prevent duplicates
     }
-    window.stockChart = new Chart(ctx, {
-      type: "line",
+    window.stockChart = new Chart(ctx, { // Creating a new Chart.js chart instance and storing it in the global window object
+      type: "line", // Setting the chart type to "line"
       data: {
-        labels: labels,
+        labels: labels, // Setting the x-axis labels for the chart
+      
         datasets: [
           {
-            label: SYMBOL,
-            data: prices,
-            fill: false,
-            borderColor: "rgb(75, 192, 192)",
-            tension: 1,
+            label: SYMBOL, 
+            data:  prices, // Setting the y-axis data for the chart
+            fill: true, // Setting the chart area to not be filled
+            lineTension: 0.1, // Setting the curve of the chart line
+            pointHoverRadius: 5,
+            pointHitRadius: 10,
+            borderColor: "#4B69FE",
+            borderWidth: "4",
+            pointRadius: 0,
+            xAxisID: "Time",
+            yAxisID: "Price",
+            
+            
+            
           },
         ],
       },
+      options: {
+        scales: {
+         
+         x: {
+          ticks:{
+            display: false,
+          }
+        },
+        y: {
+          ticks: {
+            display: false,
+          }
+        }
+
+        
+      
+        
+      }
+    }
     });
-  }, [chartData]);
+  }, [chartData]); // Specifying that the useEffect hook should only run when the chartData prop changes
+
 
   return (
-    <div>
-      <canvas id="stockChart" className="w-[500px]"></canvas>
-    </div>
+    <section className="flex justify-center items-center">
+      <div className="w-[800px] bg-white rounded-2xl px-4 border-solid border-t-4 border-r-[12px] border-b-[12px] border-l-4 border-black ">
+      <canvas id="stockChart"></canvas>
+      </div>
+      
+    </section>
   );
 };
 
--- techhub/src/pages/stockmarketnews.js
+++ techhub/src/pages/stockmarketnews.js
@@ -13,7 +13,7 @@ export async function getServerSideProps () {
 }
 
 function StockMarketNews({newsData}) {
-console.log(newsData)
+
   return (
     <div className=" flex flex-col justify-center items-center">
       <div className="bg-neoyellow py-4 mb-10 px-7 rounded-3xl border-solid border-t-4 border-r-[12px] border-b-[12px] border-l-4 border-black shadow-4xl">
@@ -21,60 +21,31 @@ console.log(newsData)
         </div>
 
 
-      {/* ARTICLE 1 */}
-
+      {/* ARTICLE */}
+{newsData.data.map((article) => (
       <a
-        href={newsData.data && newsData.data.length > 0 && newsData.data[0].url}
+        href={article.url}
         target="_blank"
+        key={article.id}
       >
         <div className=" bg-white flex justify-center content-center m-5 rounded-2xl w-[800px] border-solid border-t-2 border-r-[6px] border-b-[6px] border-l-2 border-black hover:scale-105 transition duration-300 cursor-pointer">
           <div className="flex flex-col justify-start mx-5 my-5 w-[400px]">
             {/* ARTICLE 1 TITLE */}
             <div className=" font-Oswald font-bold text-xl justify-center content-center flex">
-              {newsData.data && newsData.data.length > 0 && newsData.data[0].title}.
+              {article.title}
             </div>
 
             {/* ARTICLE 1 DESCRITION */}
             <div>
-              {newsData.data && newsData.data.length > 0 && newsData.data[0].description}
+              {article.description}
             </div>
           </div>
 
           {/* ARTICLE 1 IMG */}
           <div className="flex justify-end">
-             {newsData.data && newsData.data.length > 0 && (
+             {article.image_url && (
               <img
-                src={newsData.data[0].image_url}
-                alt="article img"
-                className="w-[300px] object-cover rounded-lg my-5 mx-5"
-              />
-            )}
-            
-          </div>
-        </div>
-      </a>
-      <a
-        href={newsData.data && newsData.data.length > 1 && newsData.data[1].url}
-        target="_blank"
-      >
-        <div className=" bg-white flex justify-center content-center m-5 rounded-2xl w-[800px] border-solid border-t-2 border-r-[6px] border-b-[6px] border-l-2 border-black  hover:scale-105 transition duration-300 cursor-pointer">
-          <div className="flex flex-col justify-start mx-5 my-5 w-[400px]">
-            {/* ARTICLE 1 TITLE */}
-            <div className=" font-Oswald font-bold text-xl justify-center content-center flex">
-              {newsData.data && newsData.data.length > 1 && newsData.data[1].title}.
-            </div>
-
-            {/* ARTICLE 1 DESCRITION */}
-            <div>
-              {newsData.data && newsData.data.length > 1 && newsData.data[1].description}
-            </div>
-          </div>
-
-          {/* ARTICLE 1 IMG */}
-          <div className="flex justify-end">
-            {newsData.data && newsData.data.length > 1 && (
-              <img
-                src={newsData.data[1].image_url}
+                src={article.image_url}
                 alt="article img"
                 className="w-[300px] object-cover rounded-lg my-5 mx-5"
               />
@@ -82,6 +53,7 @@ console.log(newsData)
           </div>
         </div>
       </a>
+             ))}
     </div>
   );
 }
